3f1479bc35f372cff8e36ea6aaa612bca7a68df5
Boyu345 <li000769@algonquinlive.com>
2023-08-06 23:35:55 -0400

    Initial A32

---
+++ A22/src/BattleshipGame/GameServer.java
@@ -0,0 +1,20 @@
+package BattleshipGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.swing.*;
+
+public class GameServer extends JFrame {
+    JCheckBox finalize;
+    JButton endButton;
+    JButton startButton;
+    JButton resultsButton;
+
+    JTextArea serverLog;
+    JTextField portFielD;
+
+
+
+}
